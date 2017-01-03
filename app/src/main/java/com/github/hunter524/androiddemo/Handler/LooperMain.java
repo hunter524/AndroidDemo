5d612e427f61813b9a9c3ed691ae40343c89cace
hunter <guyuesh2@gmail.com>
2017-01-03 22:04:22 +0800

    Demo for Looper

---
+++ app/src/main/java/com/github/hunter524/androiddemo/Handler/LooperMain.java
@@ -0,0 +1,39 @@
+package com.github.hunter524.androiddemo.Handler;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+/**
+ * Created by hunter on 2016/12/16.
+ */
+
+public class LooperMain {
+
+    public static final String TAG = "LooperMain";
+    public static class WorkThread extends Thread {
+
+        public Handler mHandler;
+
+        @Override
+        public void run() {
+            super.run();
+            Looper.prepare();
+            mHandler = new Handler(){
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d(TAG,"Threade"+Thread.currentThread());
+                    Log.d(TAG,msg.toString());
+                }
+            };
+            Looper.loop();
+        }
+    }
+}
