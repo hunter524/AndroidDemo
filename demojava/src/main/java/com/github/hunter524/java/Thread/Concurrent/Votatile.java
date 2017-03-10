6639d810e6a232a3d1d0590b32ac3f6bd664df0c
hunter <guyuesh2@gmail.com>
2017-03-10 16:34:21 +0800

    backup

---
+++ demojava/src/main/java/com/github/hunter524/java/Thread/Concurrent/Votatile.java
@@ -0,0 +1,39 @@
+package com.github.hunter524.java.Thread.Concurrent;
+
+/**
+ * Created by hunter on 2017/3/10.
+ */
+
+public class Votatile {
+    public static Long i = Long.MIN_VALUE;
+    public static void main(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    i++;
+                    System.out.println("Tread add"+"i:"+i+"time:"+System.nanoTime());
+                }
+
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    System.out.println("Tread out1"+"i:"+i+"time:"+System.nanoTime());
+                }
+
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    System.out.println("Tread out 2"+"i:"+i+"time:"+System.nanoTime());
+                }
+
+            }
+        }).start();
+    }
+}
